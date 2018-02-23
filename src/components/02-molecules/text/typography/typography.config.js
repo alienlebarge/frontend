81a26904e569b43c5cb8f627ea66728bafd9a97d
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2018-02-23 07:51:01 +0100

    refactor

---
+++ src/components/02-molecules/text/typography/typography.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+ const faker = require('faker'); // require the faker module
+ const paragraphCount = 1; // how many paragraphs we should generate data for
+ const paragraphData = [];
+
+ for (var i = 0; i < paragraphCount; i++) {
+     paragraphData.push({
+         paragraph: faker.lorem.paragraphs(), // generate a random paragraph
+     });
+ }
+
+ module.exports = {
+     context: {
+         paragraphs: paragraphData // use our generated list of members as context data for our template.
+     }
+ };
